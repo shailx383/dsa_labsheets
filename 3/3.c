ea0fccea60ba3c2c00f1b46c42ade6e088af89e4
shailx383 <65397549+shailx383@users.noreply.github.com>
2023-02-12 08:25:04 +0530

    3/3 kinda done

---
+++ 3/3.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct Node_t{
+    int data;
+    struct Node_t *next;
+    struct Node_t *prev;
+};
+typedef struct Node_t Node;
+
+Node *createNode(int value){
+    Node *new = malloc(sizeof(Node));
+    new->data = value;
+    new->prev = NULL;
+    new->next = NULL;
+}
+
+Node *addToList(Node *head, int value){
+    Node *new = createNode(value);
+    if (head == NULL){
+        return new;
+    }
+    else{
+        Node *current = head;
+        while (current->next != NULL){
+            current = current->next;
+        }
+        current->next = new;
+        new->prev = current;
+        return new;
+    }
+}
+
+Node *readList(){
+    int n;
+    scanf("%d", &n);
+    Node *head = NULL;
+    for (int i = 0; i < n; i++){
+        int x;
+        scanf("%d", &x);
+        if (head == NULL){
+            head = addToList(head, x);
+        }
+        else{
+            addToList(head, x);
+        }
+    }
+    return head;
+}
+
+void printList(Node *head){
+    Node *current = head;
+    while (current != NULL){
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+    return;
+}
+
+void freeList(Node *head){
+    Node *current, *next;
+    current = head;
+    while(current != NULL){
+        next = current->next;
+        free(current);
+        current = next;
+    }
+}
+
+int listSize(Node* head){
+    int size = 0;
+    Node *current = head;
+    while(current != NULL){
+        size++;
+        current = current->next;
+    }
+    return size;                
+}
+
+int f(Node *head, int len, int sum)
+{
+    if (sum == 0) return 1;
+    if (len == 0 && sum != 0) return 0;
+    else
+    {
+        int a = f(head->next, len - 1, sum - head->data);
+        int b = f(head->next, len - 1, sum);
+        return a || b;
+    }
+}
+
+int main(){
+    int k;
+    scanf("%d", &k);
+    Node *head = readList();
+    int n = listSize(head);
+    printf("%d", f(head, n, k));
+    freeList(head);
+}
