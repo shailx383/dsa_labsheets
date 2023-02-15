8b529efbb4e0f37c52d8642d257b271349f17c7a
shailx383 <65397549+shailx383@users.noreply.github.com>
2023-02-16 00:13:03 +0530

    fixed 10

--- 3/test.c
+++ 3/test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int num_planks(int plank_length, int arr[], int n){
+int num_planks(int arr[], int n, int plank_length){
     int sum = 0;
     for (int i = 0; i < n; i++) sum += arr[i]/plank_length;
     return sum;
@@ -9,28 +9,29 @@ int num_planks(int plank_length, int arr[], int n){
 int maxInList(int arr[], int n){
     int max = 0;
     for (int i = 0; i < n; i++){
-        if (arr[i]>max) max = arr[i];
+        if (arr[i] > max) max = arr[i];
     }
     return max;
 }
 
 int arraySum(int arr[], int n){
     int sum = 0;
-    for (int i =0; i < n; i++) sum += arr[i];
+    for (int i = 0; i < n; i++) sum += arr[i];
     return sum;
 }
 
-int binarySearch(int max, int l, int r, int target, int arr[], int n){
+int binarySearch(int max, int l, int r, int target, int n, int arr[]){
     if (arraySum(arr, n) < target) return 0;
     else{
         if (r>=l){
             int mid = l + (r-l)/2;
-            if ((num_planks(mid, arr, n) >= target) && (num_planks(mid+1, arr, n) < target)) return mid;
-            else if ((num_planks(mid, arr, n) < target)) return binarySearch(max, l, mid-1, target, arr, n);
-            else return binarySearch(max, mid+1, r, target, arr, n);
+            if ((num_planks(arr, n, mid) >= target)&&(num_planks(arr, n, mid+1) < target)) return mid;
+            else if (num_planks(arr, n, mid) < target) return binarySearch(max, l, mid-1, target, n, arr);
+            else return binarySearch(max, mid+1, r, target, n, arr);
         }
     }
 }
+
 
 int main(){
     int n, k;
@@ -38,5 +39,6 @@ int main(){
     int arr[n];
     for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
     int max = maxInList(arr, n);
-    printf("%d", binarySearch(max, 0, max, k, arr, n));
+    printf("%d", binarySearch(max, 0, max-1, k, n, arr));
+    return 0;
 }
