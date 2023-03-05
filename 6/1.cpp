6c36b5d9ad08463df6ab321f5197376d1c582522
shailx383 <shailx383@gmail.com>
2023-03-05 12:42:22 +0530

    6\1 finished

--- 6/1.cpp
+++ 6/1.cpp
@@ -1,24 +1,83 @@
 #include <iostream>
 using namespace std;
-#define size 10
 
-class MinStack {
+class Node{
+    public:
+        int data;
+        Node *next;
+        int curr_min;
+};
 
-private:
-    int top;
-    int arr[size];
+class MinStack{
+    private:
+        Node *top;
 
-public:
+    public:
+        MinStack(){
+            top = NULL;
+        }
+        
+        void push(int val){
+            Node *temp = new Node();
+            temp->data = val;
+            temp->next = top;
+            if (top) temp->curr_min = min(val, top->curr_min);
+            else temp->curr_min = val;
+            top = temp;
+        }
 
-    MinStack(){
-        top = -1;
-    }
+        void pop(){
+            Node *temp;
+            if (top == NULL){
+                cout << "Underflow" << endl;
+            }
+            else{
+                temp = top;
+                top = top->next;
+                free(temp);
+            }
+        }
 
-    void push(int val){
+        int getTop(){
+            return top->data;
+        }
 
-    }
-}
+        int getMin(){
+            return top->curr_min;
+        }
+};
 
 int main(){
+    int n;
+    cin >> n;
+    MinStack stk;
 
+    for (int i = 0; i < n; i++){
+        int val;
+        cin >> val;
+        stk.push(val);
+    }
+
+    int m;
+    cin >> m;
+
+    for (int i = 0; i < m ; i++){
+        int op;
+        cin >> op;
+        if (op == 1){
+            int val;
+            cin >> val;
+            stk.push(val);
+        }
+        else if (op == 2){
+            stk.pop();
+        }
+        else if (op == 3){
+            cout << "__" << stk.getTop() << "__" << endl;
+        }
+        else{
+            cout << "__" << stk.getMin() << "__" << endl;
+        }
+    }
+    return 0;
 }
