08073bd5efc304591ace9b183a144086c814cd88
shailx383 <shailx383@gmail.com>
2023-03-07 16:58:23 +0530

    finsihed 4,6

--- 6/4.c
+++ 6/4.c
@@ -1,65 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define int long long
 
-struct Node{
-    int data;
-    int *next;
-};
-typedef struct Node Node;
-
-Node *enqueue(Node *q, int val){
-    Node *new;
-    new->data = val;
-    new->next = NULL;
-    if (q == NULL) return new;
-    else{
-        Node *curr = q;
-        while (curr->next != NULL) curr = curr->next;
-        curr->next = new;
-        return q;
-    } 
+void initialize(int *arr, int n)
+{
+    for (int i = 0; i < n; i++) arr[i] = 0;
 }
-
-Node *dequeue(Node *q){
-    if (q->next == NULL) return NULL;
-    else{
-        Node *temp = q->next;
-        free(q);
-        return temp;
+void enqueue(int *queue, int n, int *ptr, int element, int *countarr)
+{
+    *(ptr) = *(ptr) + 1;
+    if (*ptr < n)
+    {
+        queue[*ptr] = element;
+        countarr[element]++;
     }
 }
-
-int *first_nonrepeating_integer(int arr[], int n){
-    int ans[n];
-    int *freq = (int*)malloc(sizeof(int) * n);
-    for (int i = 0; i < n; i++) freq[i] = 0;
-    Node *Q = NULL;
-    for (int i = 0 ; i < n; i++){
-        if (freq[arr[i]] == 0){
-            freq[arr[i]]++;
-            Q = enqueue(Q, arr[i]);
-            while(freq[Q->data] == 1){
-                freq[Q->data]--;
-                Q = dequeue(Q);
-            }
-        }
-        else{
-            if (Q->data == arr[i]){
-                Q = dequeue(Q);
-            }
-        }
-        if (Q != NULL) ans[i] = Q->data;
-        else ans[i] = -1; 
+int dequeue(int *queue, int *ptr, int n, int *countarr, int *front)
+{
+    while (countarr[queue[*front]] != 1)
+    {
+        *front = *front + 1;
+        if (*front > *ptr)
+            return -1;
     }
-
+    return queue[*front];
 }
 
-int main(){
+int main()
+{
     int n;
-    scanf("%d", &n);
-    int arr[n];
-    for (int i =0; i < n; i++ ) scanf("%d", &arr[i]);
-    int *result = first_nonrepeating_integer(arr, n);
-    for (int i = 0; i < n; i++) printf("%d ", result[i]);
+    scanf("%lld", &n);
+    int *countarr = malloc((n + 1) * sizeof(int));
+    initialize(countarr, n + 1);
+    int *queue = malloc(n * sizeof(int));
+    initialize(queue, n);
+    int ptr = -1, front = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int element, nonrecurring;
+        scanf("%lld", &element);
+        if (ptr == -1)
+        {
+            enqueue(queue, n, &ptr, element, countarr);
+            nonrecurring = element;
+        }
+        else if (nonrecurring == -1)
+        {
+            if (countarr[element] == 0)
+            {
+                enqueue(queue, n, &ptr, element, countarr);
+                nonrecurring = element;
+            }
+            else
+                countarr[element]++;
+        }
+        else
+        {
+            if (countarr[element] == 0)
+            {
+                enqueue(queue, n, &ptr, element, countarr);
+            }
+            else
+            {
+                countarr[element]++;
+                if (element == nonrecurring)
+                {
+                    nonrecurring = dequeue(queue, &ptr, n, countarr, &front);
+                }
+            }
+        }
+        printf("%lld ", nonrecurring);
+    }
+    free(countarr);
+    free(queue);
     return 0;
 }
