f75414d231b2651bd46148ce0809cd3da5ab271a
shailx383 <shailx383@gmail.com>
2023-04-24 10:18:31 +0530

    10/3 done

--- 10/3.cpp
+++ 10/3.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int find_median(int ar1[], int ar2[], int n, int m)
+float find_median(int ar1[], int ar2[], int n, int m)
 {
     int i = 0; 
     int j = 0; 
@@ -23,7 +23,7 @@ int find_median(int ar1[], int ar2[], int n, int m)
         return m1;
     }
     else {
-        return (m1 + m2) / 2;
+        return (m1 + m2) / 2.0;
     }
 }
 
@@ -31,11 +31,11 @@ int main(){
     int n;
     cin >> n;
     int a[n];
-    for (int i = 0; i , n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> a[i];
     int m;
     cin >> m;
     int b[m];
-    for (int i = 0; i , m; i++) cin >> b[i];
-    cout << find_median(a, n, b, m) << endl;
+    for (int i = 0; i < m; i++) cin >> b[i];
+    cout << find_median(a, b, n, m) << endl;
     return 0;
 } 
