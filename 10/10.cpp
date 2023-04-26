15a66cd0fcf1973076a2cb664d2a812060657112
shailx383 <shailx383@gmail.com>
2023-04-26 23:12:45 +0530

    finished labsheet 10

---
+++ 10/10.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int kth_smallest(vector<vector<int>> &v, int k){
+    int n = v.size();
+    priority_queue<vector<int>, vector<vector<int>>, greater<>> min_heap;
+    for (int i = 0; i < n; i++) min_heap.push({v[i][0], i, 0});
+    int ans{0};
+    for(int i = 0; i < k; i++){
+        auto top = min_heap.top();
+        min_heap.pop();
+        ans = top[0];
+        int row = top[1];
+        int col = top[2];
+        if (col + 1 < n) min_heap.push({v[row][col + 1], row, col + 1});
+    }
+    return ans;
+}
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    vector<vector<int>> v(n, vector<int>(n));
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++) cin >> v[i][j];
+    }    
+    cout << kth_smallest(v, k) << endl;
+    return 0;
+}
