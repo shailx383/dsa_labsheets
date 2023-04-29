a5b68e69879f8cfb5d3a1c35ff7ac862b68d0600
shailx383 <shailx383@gmail.com>
2023-04-29 16:32:18 +0530

    added labsheet pdfs to all

--- 10/3.cpp
+++ 10/3.cpp
@@ -1,19 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-float find_median(int ar1[], int ar2[], int n, int m){
-    int i = 0; 
-    int j = 0; 
-    int count;
-    int m1 = -1, m2 = -1;
-    for (count = 0; count <= (m + n) / 2; count++){
+float findMedian(int arr1[], int arr2[], int n, int m){
+    int count, i = 0, j = 0, m1 = -1, m2 = -1;
+    for (count = 0; count <= (m+n)/2; count++){
         m2 = m1;
-        if (i != n && j != m) m1 = (ar1[i] > ar2[j]) ? ar2[j++] : ar1[i++];        
-        else if (i < n) m1 = ar1[i++];        
-        else m1 = ar2[j++];   
-    }
-    if ((m + n) % 2 == 1) return m1;
-    else return (m1 + m2) / 2.0;
+        if (i != n && j != m) m1 = (arr1[i] > arr2[j]) ? arr2[j++] : arr1[i++];
+        else if (i < n) m1 = arr1[i++];
+        else m1 = arr2[j++];
+    } 
+    if ((m+n) % 2 == 1) return m1;
+    else return  (m1 + m2) / 2.0;
 }
 
 int main(){
@@ -25,6 +22,6 @@ int main(){
     cin >> m;
     int b[m];
     for (int i = 0; i < m; i++) cin >> b[i];
-    cout << find_median(a, b, n, m) << endl;
+    cout << findMedian(a, b, n, m) << endl;
     return 0;
-} 
+}
--- 10/test.cpp
+++ 10/test.cpp
@@ -1,12 +1,2 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
-#include <iostream>
-
 using namespace std;
-
-int main(){
-    int ans{18};
-    cout << ans << endl;
-    return 0;
-}
