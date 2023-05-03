abf9af581029ef97357b1ac108fe1b31e983d335
shailx383 <shailx383@gmail.com>
2023-05-04 01:56:19 +0530

    all dsa labsheets done

--- 11/1.cpp
+++ 11/1.cpp
@@ -1,21 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int Knapsack(int W, int index_of_nth_item, int weight[], int value[])
-{
-	if (index_of_nth_item < 0 || W == 0) return 0;
-	if (weight[index_of_nth_item] > W) return Knapsack(W, index_of_nth_item - 1, weight, value);
-	int include_nth_item = value[index_of_nth_item] + Knapsack(W - weight[index_of_nth_item], index_of_nth_item - 1, weight, value);
-	int exclude_nth_item = Knapsack(W, index_of_nth_item - 1, weight, value);
-	return max(include_nth_item, exclude_nth_item);
+int knapsack(int C, int nth_index, int weights[], int values[]){
+    if (nth_index < 0 || C == 0) return 0;
+    if (weights[nth_index] > C) return knapsack(C, nth_index-1, weights, values);
+    int include = values[nth_index] + knapsack(C - weights[nth_index], nth_index-1, weights, values);
+    int exclude = knapsack(C, nth_index-1, weights, values);
+    return max(include, exclude);     
 }
 
-int main()
-{
-    int n, W;
-    cin >> n >> W;
-	int value[n], weight[n];
-    for (int i = 0; i < n; i++) cin >> weight[i] >> value[i];
-	cout << Knapsack(W, n - 1, weight, value) << endl;
-	return 0;
-}
+int main(){
+    int n, C;
+    cin >> n >> C;
+    int weights[n], values[n];
+    for (int i = 0; i < n; i++) cin >> weights[i] >> values[i];
+    cout << knapsack(C, n-1, weights, values) << endl;
+    return 0;
+}   
