8c386be7f54399ae554b512de8f9604cecea0100
shailx383 <shailx383@gmail.com>
2023-05-04 02:20:33 +0530

    all questions in sheet 11 done

--- 11/test.cpp
+++ 11/test.cpp
@@ -1,19 +1,30 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int knapsack(int C, int nth_index, int weights[], int values[]){
-    if (nth_index < 0 || C == 0) return 0;
-    if (weights[nth_index] > C) return knapsack(C, nth_index-1, weights, values);
-    int include = values[nth_index] + knapsack(C - weights[nth_index], nth_index-1, weights, values);
-    int exclude = knapsack(C, nth_index-1, weights, values);
-    return max(include, exclude);     
+int minimum(int a, int b, int c){
+    return a < b ? (a < c ? a: c) : (b < c ? b : c);
+}
+
+int minimum_edit_distance(string a, string b, int n, int m){
+    int dp[n+1][m+1];
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= m; j++){
+            if (i ==0  || j == 0) dp[i][j] = i+j;
+            else if (a[i-1] == b[j-1]) dp[i][j] = dp[i-1][j-1];
+            else dp[i][j] = minimum(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1; 
+        }
+    }
+    return dp[n][m];
 }
 
 int main(){
-    int n, C;
-    cin >> n >> C;
-    int weights[n], values[n];
-    for (int i = 0; i < n; i++) cin >> weights[i] >> values[i];
-    cout << knapsack(C, n-1, weights, values) << endl;
+    int n, m;
+    cin >> n;
+    string s;
+    cin >> s;
+    cin >> m;
+    string t;
+    cin >> t;
+    cout << minimum_edit_distance(s, t, n, m) << endl;
     return 0;
-}   
+}
